de72b2597874de32fdeb69b2db1e047478310232
SanDmaN <73966593+MB-cyber@users.noreply.github.com>
2021-03-18 12:08:56 +0100

    Add files via upload

---
+++ BanqueAPP/CompteEpergne.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanqueAPP
+{
+    class CompteEpergne : Compte
+    {
+        private double TauxInteret;
+
+
+        public CompteEpergne(Client titu, MAD somme, double Taux) : base(titu, somme)
+        {
+            if (Taux >100.00 || Taux<0)
+            {
+                throw new ArgumentOutOfRangeException("taux invalide le taux doit etre 0<taux <100");
+            }
+            else
+            {
+                this.TauxInteret = Taux;
+            }
+
+
+        }
+
+
+        public override void consulter()
+        {
+            base.consulter();
+            Console.WriteLine("interete" + this.TauxInteret);
+        }
+         
+        public void CalcullInteret()
+        {
+            double interet = this.TauxInteret / 100;
+
+
+            this.crediter(this.multipledesolde(interet));
+
+        }
+        
+
+
+    }
+}
